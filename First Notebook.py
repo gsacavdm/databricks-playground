ee5c76af2d7eb4966f2d707bb1dc388e49179eb3
gerardo.saca@databricks.com <gerardo.saca@databricks.com>
2022-09-05 23:15:22 +0000

    Add code to access random-files

--- First Notebook.py
+++ First Notebook.py
@@ -6,3 +6,33 @@
 
 # MAGIC %md
 # MAGIC Now an edit
+
+# COMMAND ----------
+
+import os
+
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+df = spark.read.csv(f'file://{os.getcwd()}/random-files/simple.csv', header=True)
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+img = spark.read.format('image').load(f'file://{os.getcwd()}/random-files/serious-image.png')
+
+# COMMAND ----------
+
+display(img)
+
+# COMMAND ----------
+
+os.listdir('random-files')
+
+# COMMAND ----------
+
+
