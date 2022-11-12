8d621f423003b453fe3a91a0c89e34f01b74b2e6
Gerardo Saca <gsacavdm@live.com>
2022-11-12 07:14:43 -0800

    Add job for DLT Demo

--- Terraform/jobs.tf
+++ Terraform/jobs.tf
@@ -34,3 +34,22 @@ resource "databricks_job" "feature_store_nyc_taxi" {
     branch   = "master"
   }
 }
+resource "databricks_job" "dlt_demo" {
+  task {
+    task_key = "DLT_Demo_Schedule"
+    pipeline_task {
+      pipeline_id = databricks_pipeline.dlt_demo.id
+    }
+  }
+  schedule {
+    timezone_id            = "America/Los_Angeles"
+    quartz_cron_expression = "0 0 3 * * ?"
+  }
+  name                = "DLT Demo Schedule"
+  max_concurrent_runs = 1
+  email_notifications {
+    on_failure                = [data.databricks_current_user.me.user_name]
+    no_alert_for_skipped_runs = true
+  }
+}
+
