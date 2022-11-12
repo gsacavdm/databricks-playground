96f0662b7765a5f6097f2940b0731c5c05dec72c
Gerardo Saca <gsacavdm@live.com>
2022-11-11 22:37:32 -0800

    Add google-sheets job

--- Terraform/jobs.tf
+++ Terraform/jobs.tf
@@ -28,6 +28,44 @@ resource "databricks_job" "feature_store_nyc_taxi" {
   max_concurrent_runs = 1
   git_source {
     url      = "https://github.com/gsacavdm/databricks-playground"
+<<<<<<< Updated upstream
+=======
+    provider = "gitHub"
+    branch   = "master"
+  }
+}
+
+resource "databricks_job" "google_sheets" {
+  task {
+    task_key = "Google_Sheets"
+    notebook_task {
+      notebook_path = "Google Sheets Integration"
+      base_parameters = {
+        notebook_mode        = "Recurring"
+      }
+    }
+    new_cluster {
+      spark_version = "11.3.x-cpu-ml-scala2.12"
+      node_type_id = "r3.xlarge"
+      spark_conf = {
+        "spark.databricks.delta.preview.enabled" = "true"
+      }
+      runtime_engine     = "STANDARD"
+      num_workers        = 1
+      data_security_mode = "SINGLE_USER"
+    }
+    email_notifications {
+    }
+  }
+  schedule {
+    timezone_id            = "America/Los_Angeles"
+    quartz_cron_expression = "0 0 7 * * ?"
+  }
+  name                = "Google Sheets Integration"
+  max_concurrent_runs = 1
+  git_source {
+    url      = "https://github.com/gsacavdm/databricks-playground"
+>>>>>>> Stashed changes
     provider = "gitHub"
     branch   = "master"
   }
